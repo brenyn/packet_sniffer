7f32b57aca7b9c04fe4167685d278b64d2e5bb20
Brenyn <brenynk@gmail.com>
2020-05-25 03:02:24 -0400

    reading packets

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -12,3 +12,13 @@
 # Notes to self:
 #
 ##########################################################################################################
+
+import scapy.all as scapy
+
+def sniff(interface):
+	scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+	print(packet)
+
+sniff("eth0")
